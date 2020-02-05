ddf8b8faab87abe6d53093d58d9f0080b13030a4
Alex C <alejandrocuellar18@gmail.com>
2020-02-04 19:18:49 -0500

    live coding

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Class1
+{
+    var treaters = 250;
+
+    var clearWeather = treaters * 0.10;
+
+    var cloudyWeather = treaters;
+
+    var rainyWeather = treaters * -0.25;
+
+    var fullMoon = treaters * 0.25;
+
+    var fourtyDegrees = treaters * 0.95;
+
+    var fiftyDegrees = treaters;
+
+    var sixtyDegrees = treaters * 0.05;
+
+    var seventyDegrees = treaters * 0.20;
+
+    public int amountTrick(int weather, int degrees)
+    {
+        var amountCandy = (treaters *3) + weather + degrees;
+
+        return amountCandy;
+
+    }
+
+    Console.WriteLine("")
+
+}
